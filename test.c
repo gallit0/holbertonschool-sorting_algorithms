4c320e1e1992d51ab74b0b78f7029b082595245b
gallit0 <mateogesede@gmail.com>
2022-08-23 12:06:40 -0700

    task 3 finished

--- test.c
+++ test.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {98, 95, 92, 91, 87, 84, 79, 68, 62, 47, 41, 39, 34, 32, 31, 22, 21, 20, 2, 1};
+	size_t n = sizeof(array) / sizeof(array[0]);/* 20 */
 
-    print_array(array, n);
-    printf("\n");
-    quick_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	print_array(array, n);
+	printf("\n");
+	quick_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
 }
