e576ca79921f690bbe7036a473f08ba1c4607bea
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-24 11:07:19 -0700

    Fix some details in some functions

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,19 +1,63 @@
 #include "sort.h"
 
 /**
- * swap - swaps two elements in a linked list
- * @temp: list temp
- * @p: current ppointer
+ * copy_listint_node - Copy a pointer of listint_t
+ * @original: a pointer of listint_t
+ * Return: return a copy of original node
  */
-
-void swap(listint_t *temp, listint_t *p)
+listint_t *copy_listint_node(listint_t *original)
 {
-	int temp2 = 0;
+	listint_t *list;
+	int *tmp;
 
-	temp2 = temp->n;
-	*((int *)&(temp->n)) = (int)p->n;
-	*((int *)&(p->n)) = (int)temp2;
+	list = malloc(sizeof(listint_t));
+	if (!list)
+		return (NULL);
+	tmp = (int *)&list->n;
+	*tmp = original->n;
+	list->next = original->next;
+	list->prev = original->prev;
+	return (list);
 }
+/**
+ * swap_listint_node - Swap two elements of a list
+ * @list: pointer of a list
+ * @first: a pointer of listint_t
+ * @second: a pointer of listint_t
+ */
+void swap_listint_node(listint_t **list, listint_t *first, listint_t *second)
+{
+	listint_t *first_t;
+	listint_t *second_t;
+	int second_next_first = 0;
+
+	first_t = copy_listint_node(first);
+	second_t = copy_listint_node(second);
+	second_next_first = first->next == second;
+
+	first->next = second_t->next;
+	first->prev = second_next_first ? second : second_t->prev;
+	second->next = second_next_first ? first : first_t->next;
+	second->prev = first_t->prev;
+
+	if (!second_next_first)
+	{
+		first_t->next->prev = second;
+		second_t->prev->next = first;
+	}
+
+	if (first_t->prev)
+		first_t->prev->next = second;
+	if (second_t->next)
+		second_t->next->prev = first;
+
+	if (!first_t->prev)
+		*list = second;
+
+	free(first_t);
+	free(second_t);
+}
+
 
 /**
  * insertion_sort_list - instertion algorithm
@@ -31,7 +75,7 @@ void insertion_sort_list(listint_t **list)
 	{
 		if (p->n < p->prev->n)
 		{
-			swap(p, p->prev);
+			swap_listint_node(list, p->prev, p);
 			print_list(*list);
 			insertion_sort_list(list);
 			return;
