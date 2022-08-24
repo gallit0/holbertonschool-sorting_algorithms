00a817267d414a91041a579b755c60f52f09e39d
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-24 07:18:46 -0700

    Fix quick_sort function.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,17 @@
 #include "sort.h"
-#include <unistd.h>
+/**
+ * swap_array - Swap two elements in a array
+ * @first_value: a value of the array
+ * @second_value: a valua of the array
+ */
+void swap_array(int *first_value, int *second_value)
+{
+	int temp_value = 0;
 
+	temp_value = *first_value;
+	*first_value = *second_value;
+	*second_value = temp_value;
+}
 /**
  * quick_sort - quickest way to sort alogrithm
  * @array: array of ints
@@ -8,24 +19,23 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_recursion(array, size, (int)size - 1, (int)size, array);
+	quick_sort_r(array, size, (int)size, array);
 }
 /**
- * quick_sort_recursion - quickest way to sort alogrithm
+ * quick_sort_r - quickest way to sort alogrithm
  * @array: array of ints
  * @size: size of the array
- * @pivot: selected pivot
+ * @full_size: full size of original array
+ * @full_array: original array
  */
-void quick_sort_recursion(int *array, size_t size, int pivot, int full_size, int *full_array)
+void quick_sort_r(int *array, size_t size, int full_size, int *full_array)
 {
 	int i = 0;
 	int x = 0;
-	int temp = -1;
 	int has_swap = 0;
-	if ((int)size <= 0)
-	{
-		return;
-	}
+	int pivot = 0;
+
+	pivot = (int)size - 1;
 	while (i < pivot)
 	{
 		if (array[i] > array[pivot])
@@ -35,9 +45,7 @@ void quick_sort_recursion(int *array, size_t size, int pivot, int full_size, int
 			{
 				if (array[x] < array[pivot])
 				{
-					temp = array[x];
-					array[x] = array[i];
-					array[i] = temp;
+					swap_array(&array[x], &array[i]);
 					has_swap = 1;
 					print_array(full_array, full_size);
 					break;
@@ -45,29 +53,18 @@ void quick_sort_recursion(int *array, size_t size, int pivot, int full_size, int
 			}
 			if (!has_swap)
 			{
-				temp = array[i];
-				array[i] = array[pivot];
-				array[pivot] = temp;
+				swap_array(&array[i], &array[pivot]);
 				has_swap = 1;
 				pivot = i;
 				print_array(full_array, full_size);
 				break;
-			}	
+			}
 		}
 		i++;
 	}
-	if ((int)size > 0 && pivot < (int)size)
-	{
-		if (pivot > 1 && (pivot - 1) < ((int)size))
-		{
-			quick_sort_recursion(array, pivot, pivot - 1, full_size, full_array);
-		}
+	if (pivot > 0)
+		quick_sort_r(array, pivot, full_size, full_array);
 
-		if ((int)size - (i + 1) > 0 && pivot < ((int)size) && has_swap)
-		{
-			int new_size = 0;
-			new_size = (int)size - (pivot + 2);
-			quick_sort_recursion(&array[i + 1], size - (pivot + 1), new_size, full_size, full_array);
-		}
-	}
+	if ((int)size - (pivot + 1) > 0)
+		quick_sort_r(&array[pivot + 1], size - (pivot + 1), full_size, full_array);
 }
--- sort.h
+++ sort.h
@@ -23,5 +23,5 @@ void insertion_sort_list(listint_t **list);
 void swap(listint_t *temp, listint_t *p);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-void quick_sort_recursion(int *array, size_t size, int pivot, int full_size, int *full_array);
+void quick_sort_r(int *array, size_t size, int full_size, int *full_array);
 #endif
